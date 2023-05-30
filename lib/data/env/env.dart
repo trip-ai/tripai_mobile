e201f4630cd49c18c6413cc2eec0085b2b415e62
Bobir Abdukhalilov <destbest94@gmail.com>
2023-05-30 15:06:46 +0500

    add: App environment;

---
+++ lib/data/env/env.dart
@@ -0,0 +1,5 @@
+class AppEnvironment {
+  static const String weatherApiKey = 'c30856a23828426c931153517232405';
+  static const String weatherApi =
+      'https://api.weatherapi.com/v1/current.json?key=c30856a23828426c931153517232405';
+}
